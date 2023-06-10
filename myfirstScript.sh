ae3e495fe3bce7e4a6f665070d87279c54c4af95
NQ-bit <86328600+NQ-bit@users.noreply.github.com>
2023-06-10 17:30:13 -0400

    Update myfirstScript.sh

--- myfirstScript.sh
+++ myfirstScript.sh
@@ -1,1 +1,17 @@
 #This is my first command. 
+
+#Update OS 
+sudo apt update -y 
+
+#Installs MySQL
+sudo apt install mysql-server 
+
+#Installs MongoDB
+sudo apt install mongodb-org
+
+#Installs Apache 
+sudo apt install apache2
+
+echo Done!
+
+exit 0 
